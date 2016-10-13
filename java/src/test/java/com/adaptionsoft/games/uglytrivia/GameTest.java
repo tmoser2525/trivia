4c3698366466847aa5d0c241bffa01a322b840a0
Clay Dowling <clay@lazarusid.com>
2016-10-12 21:41:52 -0400

    Start of safety net around currentCategory and askQuestion

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/GameTest.java
@@ -1,10 +1,16 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.containsString;
 
 /**
  * Created by clay on 10/12/16.
@@ -12,11 +18,29 @@ import static org.junit.Assert.*;
 public class GameTest {
 
     Game game;
+    private ByteArrayOutputStream baos;
+    private PrintStream oldPrintStream;
 
     @Before
     public void setUp()
     {
         game = new Game();
+
+        // From StackOverflow code by Ernest Friedman-Hill
+        // Create a stream to hold the output
+        baos = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(baos);
+        // IMPORTANT: Save the old System.out!
+        oldPrintStream = System.out;
+        // Tell Java to use your special stream
+        System.setOut(ps);
+    }
+
+    @After
+    public void tearDown()
+    {
+        // Undo outstream capture in setUp
+        System.setOut(oldPrintStream);
     }
 
     @Test
@@ -36,4 +60,31 @@ public class GameTest {
         assertThat(game.scienceQuestions.get(33).toString(), is("Science Question 33"));
         assertThat(game.sportsQuestions.get(21).toString(), is("Sports Question 21"));
     }
+
+    private void setupTwoPlayers()
+    {
+        game.add("Alice");
+        game.add("Bob");
+    }
+
+    @Test
+    public void roll_withCurrentPlayerInPlace0_asksScienceQuestion()
+    {
+        setupTwoPlayers();
+        game.currentPlayer = 0;
+        game.places[game.currentPlayer] = 0;
+        game.roll(1);
+        assertThat(baos.toString(), containsString("Science Question 0") );
+    }
+
+    @Test
+    public void roll_withCurrentPlayerInPlace0_asksSportsQuestion()
+    {
+        setupTwoPlayers();
+        game.currentPlayer = 0;
+        game.places[game.currentPlayer] = 1;
+        game.roll(1);
+        assertThat(baos.toString(), containsString("Sports Question 0") );
+    }
+
 }
